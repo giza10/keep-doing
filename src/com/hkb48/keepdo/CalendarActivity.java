0b00a97a1a3cc699a7c5589420979b96324bb59c
Takamasa Kuramitsu <kurakura.7077@gmail.com>
2012-11-22 08:01:41 +0900

    Update calendar view.

--- src/com/hkb48/keepdo/CalendarActivity.java
+++ src/com/hkb48/keepdo/CalendarActivity.java
@@ -51,7 +51,7 @@ public class CalendarActivity extends MainActivity {
         TextView textView1 = (TextView) findViewById(R.id.textView1);
         textView1.setText(sdf.format(calendar.getTime()));
     }
-    
+
     /***
      * Build calendar view
      */
@@ -106,31 +106,49 @@ public class CalendarActivity extends MainActivity {
      */
     private void addDayOfMonth(Calendar calendar) {
         int maxdate = calendar.getMaximum(Calendar.DAY_OF_MONTH);
+        int week = calendar.get(Calendar.DAY_OF_WEEK);
 
-        for (int i = 0; i < maxdate; i++) {
+        // Fill the days of previous month in the first week with blank rectangle
+        for (int i = 0; i < (week - Calendar.SUNDAY); i++) {
+            GridLayout.LayoutParams params = new GridLayout.LayoutParams();
+            params.rowSpec = GridLayout.spec(1);
+            params.columnSpec = GridLayout.spec(i);
+            View child = getLayoutInflater().inflate(R.layout.calendar_date, null);
+            child.setBackgroundResource(R.drawable.bg_calendar_day_blank);
+            mGridLayout.addView(child, params);
+        }
+
+        for (int date = 1; date <= maxdate; date++) {
             View child = getLayoutInflater().inflate(R.layout.calendar_date, null);
             TextView textView1 = (TextView) child.findViewById(R.id.textView1);
             ImageView imageView1 = (ImageView) child.findViewById(R.id.imageView1);
-            int week = calendar.get(Calendar.DAY_OF_WEEK);
+            week = calendar.get(Calendar.DAY_OF_WEEK);
             int fontColorOfWeek = getFontColorOfWeek(week);
 
-            textView1.setText(Integer.toString(i + 1));
+            textView1.setText(Integer.toString(date));
             textView1.setTextColor(fontColorOfWeek);
 
             // Tentative
-            if (i%10 != 1) {
-                imageView1.setVisibility(View.GONE);
+            if (date%10 == 1) {
+                imageView1.setVisibility(View.VISIBLE);
+            }
+            if (week == Calendar.WEDNESDAY) {
+                child.setBackgroundResource(R.drawable.bg_calendar_day_off);
             }
 
             GridLayout.LayoutParams params = new GridLayout.LayoutParams();
-            if (i == 0) {
-                params.rowSpec = GridLayout.spec(1);
-                params.columnSpec = GridLayout.spec(week - Calendar.SUNDAY);
-            }
             params.setGravity(Gravity.FILL_HORIZONTAL);
             mGridLayout.addView(child, params);
 
             calendar.add(Calendar.DAY_OF_MONTH, 1);
+        }
+
+        // Fill the days of next month in the last week with blank rectangle
+        for (int i = 0; i < (7 - week); i++) {
+            GridLayout.LayoutParams params = new GridLayout.LayoutParams();
+            View child = getLayoutInflater().inflate(R.layout.calendar_date, null);
+            child.setBackgroundResource(R.drawable.bg_calendar_day_blank);
+            mGridLayout.addView(child, params);
         }
     }
 
