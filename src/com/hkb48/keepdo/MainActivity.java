65eca52b2bee9350c6b488ad7864ee9d34de3927
unknown <Bilige Bao@HOME-VAIO.(none)>
2012-11-22 00:34:32 +0900

    miner update (2) for DB, not yet been tested

--- src/com/hkb48/keepdo/MainActivity.java
+++ src/com/hkb48/keepdo/MainActivity.java
@@ -5,6 +5,7 @@ import java.util.Date;
 
 import android.app.Activity;
 import android.content.ContentValues;
+import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
 import android.view.Menu;
 
@@ -41,23 +42,45 @@ public class MainActivity extends Activity {
 
 	}
 
-	protected void addTask(String taskName, String frequency) {
-		ContentValues cvTask = new ContentValues(2);
-		cvTask.put(TasksToday.TASK_NAME, taskName);
-		cvTask.put(TasksToday.TASK_FREQUENCY, frequency);
-		long _id = mDatabaseHelper.getWritableDatabase().insert(TasksToday.TASK_NAME, null, cvTask);
+	protected void addTask(String taskName, Integer frequency) {
 
-		ContentValues cvComplete = new ContentValues();
-		cvComplete.put(TaskCompletions.TASK_NAME_ID, _id);
-        mDatabaseHelper.getWritableDatabase().insert(TaskCompletions.TASK_COMPLETION_TABLE_NAME, null, cvComplete);
-        mDatabaseHelper.close();
+		if ((taskName ==null) || (taskName.isEmpty()) || (frequency == null)) {
+			return;
+		}
+
+		try {
+			ContentValues cvTask = new ContentValues(2);
+			cvTask.put(TasksToday.TASK_NAME, taskName);
+			cvTask.put(TasksToday.TASK_FREQUENCY, frequency.toString());
+			long rowID = mDatabaseHelper.getWritableDatabase().insert(TasksToday.TASK_NAME, null, cvTask);
+
+			ContentValues cvComplete = new ContentValues();
+			cvComplete.put(TaskCompletions.TASK_NAME_ID, rowID);
+			mDatabaseHelper.getWritableDatabase().insert(TaskCompletions.TASK_COMPLETION_TABLE_NAME, null, cvComplete);
+
+			mDatabaseHelper.close();
+		} catch (SQLiteException e) {
+			//@ToDo.
+		}
 	}
 
-	protected void checkDone(Long taskID) {
-		ContentValues cv = new ContentValues();
-	    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-	    cv.put(TaskCompletions.TASK_COMPLETION_DATE, dateFormat.format(new Date())); //Insert 'now' as the date
-	    mDatabaseHelper.getWritableDatabase().update(TaskCompletions.TASK_COMPLETION_TABLE_NAME, cv, "id=?", new String[] {taskID.toString()});
-	    mDatabaseHelper.getWritableDatabase().close();
+	protected void checkDone(Long taskID, Boolean doneSwitch) {
+		if (taskID ==null) {
+			return;
+		}
+
+		try {
+			ContentValues cv = new ContentValues();
+			if (doneSwitch == true) {				
+			    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+			    cv.put(TaskCompletions.TASK_COMPLETION_DATE, dateFormat.format(new Date())); //Insert 'now' as the date
+			} else {
+				cv.putNull(null);
+			}
+		    mDatabaseHelper.getWritableDatabase().update(TaskCompletions.TASK_COMPLETION_TABLE_NAME, cv, "id=?", new String[] {taskID.toString()});
+		    mDatabaseHelper.getWritableDatabase().close();
+		} catch (SQLiteException e) {
+			//@Todo.
+		}
 	}
 }
