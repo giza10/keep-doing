66ee29f39af2b5da380f851f60c06acb62cabfc2
Takamasa Kuramitsu <kurakura.7077@gmail.com>
2013-02-07 01:48:47 +0900

    Implementation of generic alert setting (final)

--- src/com/hkb48/keepdo/AlertPreferences.java
+++ src/com/hkb48/keepdo/AlertPreferences.java
@@ -1,15 +1,26 @@
 package com.hkb48.keepdo;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
 
-public class AlertPreferences extends PreferenceFragment {
+public class AlertPreferences extends PreferenceFragment implements OnPreferenceChangeListener {
 
     private static final String SHARED_PREFS_NAME = "com.hkb48.keepdo_preferences";
 
+    public static final String KEY_ALERTS_CATEGORY = "preferences_alerts_category";
     public static final String KEY_ALERTS_RINGTONE = "preferences_alerts_ringtone";
+    public static final String KEY_ALERTS_VIBRATE_WHEN = "preferences_alerts_vibrateWhen";
+
+    ListPreference mVibrateWhen;
 
     public static SharedPreferences getSharedPreferences(Context context) {
         return context.getSharedPreferences(SHARED_PREFS_NAME, Context.MODE_PRIVATE);
@@ -20,5 +31,36 @@ public class AlertPreferences extends PreferenceFragment {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.alert_preferences);
+
+        final Activity activity = getActivity();
+        final PreferenceScreen preferenceScreen = getPreferenceScreen();
+        mVibrateWhen = (ListPreference) preferenceScreen.findPreference(KEY_ALERTS_VIBRATE_WHEN);
+        Vibrator vibrator = (Vibrator) activity.getSystemService(Context.VIBRATOR_SERVICE);
+        if (vibrator == null || !vibrator.hasVibrator()) {
+            PreferenceCategory alertGroup = (PreferenceCategory) preferenceScreen.findPreference(KEY_ALERTS_CATEGORY);
+            alertGroup.removePreference(mVibrateWhen);
+        } else {
+            mVibrateWhen.setSummary(mVibrateWhen.getEntry());
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mVibrateWhen.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mVibrateWhen.setOnPreferenceChangeListener(null);
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mVibrateWhen) {
+            mVibrateWhen.setValue((String)newValue);
+            mVibrateWhen.setSummary(mVibrateWhen.getEntry());
+        }
+        return false;
     }
 }
--- src/com/hkb48/keepdo/RemindAlarmReceiver.java
+++ src/com/hkb48/keepdo/RemindAlarmReceiver.java
@@ -9,6 +9,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.AudioManager;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
@@ -55,6 +56,16 @@ public class RemindAlarmReceiver extends BroadcastReceiver {
         String reminderRingtone = prefs.getString(AlertPreferences.KEY_ALERTS_RINGTONE, null);
         if (reminderRingtone != null) {
             builder.setSound(Uri.parse(reminderRingtone));
+        }
+        String vibrateWhen = prefs.getString(AlertPreferences.KEY_ALERTS_VIBRATE_WHEN, null);
+        boolean vibrateAlways = vibrateWhen.equals("always");
+        boolean vibrateSilent = vibrateWhen.equals("silent");
+        AudioManager audioManager = (AudioManager)context.getSystemService(Context.AUDIO_SERVICE);
+        boolean nowSilent = audioManager.getRingerMode() == AudioManager.RINGER_MODE_VIBRATE;
+
+        // Possibly generate a vibration
+        if (vibrateAlways || (vibrateSilent && nowSilent)) {
+            builder.setDefaults(Notification.DEFAULT_VIBRATE);
         }
         Intent newIntent = new Intent(context, TasksActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, newIntent, 0);
