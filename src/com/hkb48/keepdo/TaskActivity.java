f7fc6a2237cfdc51be13475259a63ce1469c44a6
Takamasa Kuramitsu <kurakura.7077@gmail.com>
2013-08-01 00:52:44 +0900

    Insert tabs in Action bar for Landscape

--- src/com/hkb48/keepdo/TaskActivity.java
+++ src/com/hkb48/keepdo/TaskActivity.java
@@ -1,45 +1,65 @@
 package com.hkb48.keepdo;
 
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentTabHost;
 import android.view.MenuItem;
-import android.widget.TabHost;
 
-public class TaskActivity extends FragmentActivity {
-
-    private String mCurrentSelectedTab;
+public class TaskActivity extends FragmentActivity implements
+        ActionBar.TabListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task);
 
-        FragmentTabHost mTabHost = new FragmentTabHost(this);
-        setContentView(mTabHost);
+        final ActionBar actionBar = getActionBar();
+        assert actionBar != null;
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
         final long taskId = getIntent().getLongExtra("TASK-ID", -1);
         final Task task = DatabaseAdapter.getInstance(this).getTask(taskId);
-        if (task != null) setTitle(task.getName());
+        if (task != null)
+            setTitle(task.getName());
 
-        mTabHost.setup(this, getSupportFragmentManager(), R.layout.activity_task);
-        mTabHost.addTab(mTabHost.newTabSpec(getResources().getString(R.string.title_section1)).setIndicator(getResources().getString(R.string.title_section1)), CalendarFragment.class, null);
-        mTabHost.addTab(mTabHost.newTabSpec(getResources().getString(R.string.title_section2)).setIndicator(getResources().getString(R.string.title_section2)), TaskDetailFragment.class, null);
-        mTabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
-            public void onTabChanged(String tabId) {
-                mCurrentSelectedTab = tabId;
-            }
-        });
+        // For each of the sections in the app, add a tab to the action bar.
+        actionBar.addTab(actionBar.newTab().setText(R.string.title_section1)
+                .setTabListener(this));
+        actionBar.addTab(actionBar.newTab().setText(R.string.title_section2)
+                .setTabListener(this));
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case android.R.id.home:
-                finish();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+        case android.R.id.home:
+            finish();
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
         }
+    }
+
+    public void onTabReselected(Tab arg0, FragmentTransaction ft) {
+        // TODO Auto-generated method stub
+    }
+
+    public void onTabSelected(Tab tab, FragmentTransaction ft) {
+        if (tab.getPosition() == 0) {
+            CalendarFragment fragment = new CalendarFragment();
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, fragment).commit();
+        } else {
+            TaskDetailFragment fragment = new TaskDetailFragment();
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, fragment).commit();
+        }
+    }
+
+    public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+        // TODO Auto-generated method stub
     }
 }
