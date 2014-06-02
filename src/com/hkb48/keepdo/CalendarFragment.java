6b9da50a1540f15013381af449dc8209582813a5
Takamasa Kuramitsu <kurakura.7077@gmail.com>
2014-06-02 23:30:32 +0900

    Fix IllegalAccessException on CalendarFlagment

--- src/com/hkb48/keepdo/CalendarFragment.java
+++ src/com/hkb48/keepdo/CalendarFragment.java
@@ -9,7 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-class CalendarFragment extends Fragment {
+public class CalendarFragment extends Fragment {
 
     /**
      * The maximum number of months to be paging up (10 years)
