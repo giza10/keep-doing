01cb23754b21f43d056dd8b8d78bf5eba44cbf14
Takamasa Kuramitsu <kurakura.7077@gmail.com>
2015-11-21 23:42:12 +0900

    Update version of support libraries.

--- app/src/main/java/com/hkb48/keepdo/com/hkb48/keepdo/util/CompatUtil.java
+++ app/src/main/java/com/hkb48/keepdo/com/hkb48/keepdo/util/CompatUtil.java
@@ -5,11 +5,12 @@ import android.os.Build;
 import android.support.v4.content.ContextCompat;
 
 public class CompatUtil {
-    public static final int getColor(Context context, int id) {
+    public static int getColor(Context context, int id) {
         final int version = Build.VERSION.SDK_INT;
         if (version >= 23) {
             return ContextCompat.getColor(context, id);
         } else {
+            //noinspection deprecation
             return context.getResources().getColor(id);
         }
     }
